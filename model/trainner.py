e724509859a6b32cfbfb73967557d911d4d05a32
Caio Andrade <caiotdandrade@gmail.com>
2021-10-26 18:59:14 -0300

    wip: implementing training loop
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- model/trainner.py
+++ model/trainner.py
@@ -2,11 +2,37 @@ from preprocess_dataset import NERDataset
 from torch.utils.data import DataLoader
 from datasets import load_dataset
 
+import torch
+import numpy as np
+
 def create_data_loader(df, max_len, batch_size, num_workers):
     ds = NERDataset(df, max_len)
     return DataLoader(ds, batch_size=batch_size, num_workers=num_workers)
 
+class Trainner:
 
+    def __init__(self, device, dataLoader, model, optimizer):
+        self.model = model.to(device)
+        self.dataLoader = dataLoader
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.total_steps = len(dataLoader) * epochs
+
+    def _train_epoch(self):
+        model = self.model.train()
+
+        losses = []
+        correct = 0
+
+        for sample in self.dataLoader:
+            input_tensor = sample["input_ids"]
+            att_mask = sample["attention_mask"]
+            target = sample["targets"]
+
+            loss, output = model(ids=input, mask=att_mask, labels=target)
+            losses.append(loss)
+        
+        return np.mean(losses)
 
 def main():
 
