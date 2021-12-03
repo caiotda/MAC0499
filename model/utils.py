27aac10e9965dfd10c58324404fa7edb0958c102
Caio Andrade <caiotdandrade@gmail.com>
2021-12-02 21:11:43 -0300

    feat/refactor: implements preds2labels for batch
    - converts logits to label for whole batch. Accuracy can now be
    calculated in train time.
    
    - refactors preds_to_label_idx to use extract_max
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- model/utils.py
+++ model/utils.py
@@ -7,8 +7,15 @@ def extract_max(sentence):
     return [torch.argmax(word).item() for word in sentence]
 
 def preds_to_label_idx(logits):
+    """
+    Given an torch.tensor of logits of shape (NUM_WORDS, NUM_LABELS),
+    outputs the predictions.
+    """
     s = Sigmoid()
     probs = s(logits)
-    preds = torch.tensor([torch.argmax(prob).item() for prob in probs])
+    preds = torch.tensor(extract_max(probs))
 
     return preds
+
+def batch_to_labels(batch_logits):
+    return torch.tensor([extract_max(logits) for logits in batch_logits])
