bbca6cdd8c426f2dab982a0fb0fd78c406ac1480
Caio Andrade <caiotdandrade@gmail.com>
2021-12-24 17:06:51 -0300

    feat: adds script that evaluates model in test set
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

---
+++ model/evaluator.py
@@ -0,0 +1,45 @@
+from preprocess_dataset import NERDataset
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+
+from utils import batch_to_labels, batch_f1
+
+import torch
+import numpy as np
+
+
+def evaluate(model, ds_test, dev):
+    """
+    Given a refined model, a test dataset and a number of epochs, 
+    evaluat
+    """
+    
+    model.eval()
+    
+    
+    debug_ammount = 100
+    losses = []
+    f1_l = []
+    temp_f1 = []
+
+    with torch.no_grad():
+        for idx, sample in enumerate(ds_test):
+
+                input_tensor = sample["input_ids"].squeeze().to(dev, dtype = torch.long)
+                att_mask = sample["attention_mask"].to(dev, dtype = torch.long)
+                target = sample["targets"].to(dev, dtype = torch.long)
+
+                out = model(input_tensor, att_mask, labels=target)
+                logits = out['logits']
+                loss = out['loss']
+                f1 = batch_f1(true=target, logits=logits)
+                
+                losses.append(loss.item())
+                f1_l.append(f1)
+                temp_f1.append(f1)
+                
+                if (idx % debug_ammount == 0 and idx != 0):
+                    prog = "{:.2f}".format(100*idx/len(ds_test))
+                    print(f"Iteração {idx} -------- Loss: {loss} f1 nas ultimas {debug_ammount} iterações: {np.sum(temp_f1)/debug_ammount} ------ Progresso: {prog}%.")
+                    temp_f1 = []
+    return losses, f1_l
